7ad15d0cb8fd8d9a9f129cfae6cc05a05032c17e
tharouet <60984127+tharouet@users.noreply.github.com>
2021-07-30 18:55:39 +0100

    request the minium number of ints per progression

--- silvercipher.go
+++ silvercipher.go
@@ -7,30 +7,29 @@ import "fmt"
 
 // Find Progression find int number progression either on an ascending or descending basis
 // and return a map of slices with the results
-// operators can either be "desc" or "asc"
-func FindProgression(d []int, operators string) (r map[int][]int, err error) {
+// operator can either be "desc" or "asc"
+// minimum is an int and it the minimum number of numbers progressing int he slice
+func FindProgression(d []int, operator string, minimum int) (r map[int][]int, err error) {
+
+	// set parameters
 	r = make(map[int][]int)
 	f := make([]int, 0)
 	i := 0
+
+	// minium number of ints in a progression
+	if minimum < 1 {
+		err = fmt.Errorf("invalid minimum input: %v ! minimum value must but 1 or over ", minimum)
+		return nil, err
+	}
+
+	minimum = minimum - 1
 	f = append(f, d[0])
-	if operators == "asc" {
+	if operator == "asc" {
 		for k := 1; k < len(d); k++ {
 			if d[k-1] < d[k] {
 				f = append(f, d[k])
 			} else {
-				r[i] = f
-				i++
-				f = []int{}
-				f = append(f, d[k])
-			}
-		}
-		r[i] = f
-	} else if operators == "desc" {
-		for k := 1; k < len(d); k++ {
-			if d[k-1] > d[k] {
-				f = append(f, d[k])
-			} else {
-				if len(f) > 0 {
+				if len(f) > minimum {
 					r[i] = f
 					i++
 				}
@@ -38,9 +37,27 @@ func FindProgression(d []int, operators string) (r map[int][]int, err error) {
 				f = append(f, d[k])
 			}
 		}
-		r[i] = f
+		if len(f) > minimum {
+			r[i] = f
+		}
+	} else if operator == "desc" {
+		for k := 1; k < len(d); k++ {
+			if d[k-1] > d[k] {
+				f = append(f, d[k])
+			} else {
+				if len(f) > minimum {
+					r[i] = f
+					i++
+				}
+				f = []int{}
+				f = append(f, d[k])
+			}
+		}
+		if len(f) > minimum {
+			r[i] = f
+		}
 	} else {
-		err = fmt.Errorf("invalid input: <%v> ! operator type can only be desc or asc", operators)
+		err = fmt.Errorf("invalid operator input: %v ! operator type can only be desc or asc", operator)
 		return r, err
 	}
 	return r, nil
